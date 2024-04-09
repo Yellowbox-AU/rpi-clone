7fddfc9bbb626c320834dd0d6d1e0f92fdbccb5b
Christopher Fraser <chris@yellowboxglobal.com>
2024-04-10 00:36:48 +1000

    Make rpi-clone script clearer to invoke

--- test.sh
+++ test.sh
@@ -2,16 +2,17 @@
 
 # TODO: Check both namespace and start id are specified
 
-if [ "$1" == "" ] || [ "$2" == "" ]
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ];
 then
-    echo "Missing required args. Usage: $0 <namespace> <startid>"
+    echo "Missing required args. Usage: $0 <namespace> <startid> <firebase-project>"
     exit 1
 fi
 
 namespace=$1
 i=$2
+firebase_project=$3
 set -x
 for d in $(echo /dev/sd?); do
-    sudo ./rpi-clone "${d//\/dev\//}" -s "$namespace-$i" -U &
+    sudo ./rpi-clone "${d//\/dev\//}" "$namespace-$i" "$firebase_project" -U &
     i=$((i + 1))
 done
